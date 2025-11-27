1e4348ffc334db0fe31086d4caa7dda3ee285ea5
bugs <234222906+bewgsy1@users.noreply.github.com>
2025-11-27 12:29:54 +0000

    Added non fixed value to check for prime number

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ function isPrime(n) {
         if(n % x === 0) {
             return;
         }
-
     }
         console.log(`This is a prime number ${n}\n`)
         return;
 
 }
 
-for(let x = 2; x < 150; x++) {
+// dynamic non fixed number
+let input = 10;
+
+for(let x = 2; x < input; x++) {
     isPrime(x);
 }
